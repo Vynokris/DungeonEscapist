bb346c6d3e628e9a48e0cc24eba3ccf4c7f2cb90
Vynokris <vynokris@gmail.com>
2022-10-24 14:15:08 +0200

    :tada: Brawler initialization

---
+++ Source/UnrealBrawler/UnrealBrawlerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBrawlerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealBrawlerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealBrawlerGameMode();
+};
+
+
+
