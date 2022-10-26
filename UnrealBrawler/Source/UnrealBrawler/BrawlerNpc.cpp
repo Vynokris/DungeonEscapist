84ef43410db4b41e549dce6130ba74e77ce02218
Vynokris <vynokris@gmail.com>
2022-10-26 17:58:45 +0200

    :twisted_rightwards_arrows: Merge

--- UnrealBrawler/Source/UnrealBrawler/BrawlerNpc.cpp
+++ UnrealBrawler/Source/UnrealBrawler/BrawlerNpc.cpp
@@ -5,6 +5,9 @@ ABrawlerNpc::ABrawlerNpc()
 {
  	// Set this character to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Get a random strafe direction.
+    StrafeDir = FMath::RandRange(0, 1) == 0 ? 1 : -1;
 }
 
 // Called when the game starts or when spawned.
@@ -14,7 +17,7 @@ void ABrawlerNpc::BeginPlay()
 }
 
 // Called every frame.
-void ABrawlerNpc::Tick(float DeltaTime)
+void ABrawlerNpc::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
@@ -23,5 +26,11 @@ void ABrawlerNpc::Tick(float DeltaTime)
 void ABrawlerNpc::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+// Returns the AI's strafe direction (1 : right, -1 : left).
+int ABrawlerNpc::GetStrafeDir() const
+{
+	return StrafeDir;
 }
 
--- UnrealBrawler/Source/UnrealBrawler/BrawlerNpc.h
+++ UnrealBrawler/Source/UnrealBrawler/BrawlerNpc.h
@@ -9,18 +9,23 @@ class UNREALBRAWLER_API ABrawlerNpc : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties.
-	ABrawlerNpc();
-
 protected:
+	// The direction in which the AI strafes when the player isn't visible (1 : right, -1 : left).
+	int StrafeDir;
+	
 	// Called when the game starts or when spawned.
 	virtual void BeginPlay() override;
 
 public:	
+	// Sets default values for this character's properties.
+	ABrawlerNpc();
+	
 	// Called every frame.
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(const float DeltaTime) override;
 
 	// Called to bind functionality to input.
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Returns the AI's strafe direction (1 : right, -1 : left).
+	int GetStrafeDir() const;
 };
